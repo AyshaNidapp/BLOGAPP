ad4a74fc0067e549c16be0a7dae99bf818873bf6
AyshaNidapp <165902463+AyshaNidapp@users.noreply.github.com>
2024-04-21 19:52:27 +0530

    Add files via upload

---
+++ front/src/assets/data.js
@@ -0,0 +1,155 @@
+export const nav = [
+    {
+      id: 1,
+      text: "home",
+      url: "/",
+    },
+    {
+      id: 2,
+      text: "about",
+      url: "/about",
+    },
+    {
+      id: 3,
+      text: "pages",
+      url: "/pages",
+    },
+    {
+      id: 4,
+      text: "blog",
+      url: "/blog",
+    },
+    {
+      id: 5,
+      text: "contact",
+      url: "/contact",
+    },
+  ]
+  export const category = [
+    {
+      id: 1,
+      category: "CAT",
+      title: "MEOW",
+      cover: "/p1.jpg",
+    },
+    {
+      id: 2,
+      category: "DOG",
+      title: "BOWBOW",
+      cover: "/p2.jpg",
+    },
+    {
+      id: 3,
+      category: "RABBIT",
+      title: "CLUCK",
+      cover: "/p3.jpg",
+    },
+    {
+      id: 4,
+      category: "BIRDS",
+      title: "TSSSHHK",
+      cover: "/p4.jpg",
+    },
+    {
+      id: 5,
+      category: "HORSE",
+      title: "NEIJH",
+      cover: "/p5.jpg",
+    },
+    {
+      id: 6,
+      category: "FISH",
+      title: "CLICKING",
+      cover: "/p6.jpg",
+    },
+    {
+      id: 7,
+      category: "DUCK",
+      title: "QUACKQUACK",
+      cover: "/p7.jpg",
+    },
+    {
+      id: 8,
+      category: "SHEEP",
+      title: "BLEAT",
+      cover: "/p8.jpg",
+    },
+  ]
+  export const blog = [
+    {
+      id: 1,
+      title: "Freedom never felt so good-birds",
+      desc: "Birds are a group of warm-blooded vertebrates constituting the class Aves, characterised by feathers, toothless beaked jaws, the laying of hard-shelled eggs, a high metabolic rate, a four-chambered heart, and a strong yet lightweight skeleton.",
+      category: "BIRDS",
+      cover: "/p12.jpg",
+      date: "APRIL 05, 2024",
+    },
+  
+    {
+      id: 2,
+      title: "Feeling ducky today",
+      desc:  "Duck is the common name for numerous species of waterfowl in the family Anatidae. Ducks are generally smaller and shorter-necked than swans and geese, which are members of the same family.",
+      category: "DUCK",
+      cover: "/p9.jpg",
+      date: "APRIL 05, 2024",
+    },
+    {
+      id: 3,
+      title: "My best friend has four paws-dog",
+      desc: "The dog is a domesticated descendant of the wolf. Also called the domestic dog, it is derived from extinct gray wolves, and the gray wolf is the dog's closest living relative. The dog was the first species to be domesticated by humans",
+      category: "DOG",
+      cover: "/p14.jpg",
+      date: "MARCH 25, 2024",
+    },
+    {
+      id: 4,
+      title: "Meet my four legged furry love-horse",
+      desc: "The horse is a domesticated, one-toed, hoofed mammal. It belongs to the taxonomic family Equidae and is one of two extant subspecies of Equus ferus. The horse has evolved over the past 45 to 55 million years from a small multi-toed creature, close to Eohippus, into the large, single-toed animal of today.",
+      category: "HORSE",
+      cover: "/p11.jpg",
+      date: "APRIL 04, 2024",
+    },
+    {
+      id: 5,
+      title: "Walking on woolen clouds-sheeps",
+      desc: "Sheep or domestic sheep are a domesticated, ruminant mammal typically kept as livestock. Although the term sheep can apply to other species in the genus Ovis, in everyday usage it almost always refers to domesticated sheep. Like all ruminants, sheep are members of the order Artiodactyla, the even-toed ungulates.",
+      category: "SHEEP",
+      cover: "/p16.jpg",
+      date: "APRIL 02, 2024",
+    },
+    {
+      id: 6,
+      title: "This little ball of fur stole my heart!-rabbit",
+      desc: "Rabbits, also known as bunnies or bunny rabbits, are small mammals in the family Leporidae, which is in the order Lagomorpha. Oryctolagus cuniculus is the European rabbit, including its descendants, the world's 305 breeds of domestic rabbit.",
+      category: "RABBIT",
+      cover: "/o.jpg",
+      date:  "APRIL 03, 2024",
+    },
+    {
+      id: 7,
+      title: "Keep calm and fish on",
+      desc: "A fish is an aquatic, anamniotic, gill-bearing vertebrate animal with swimming fins and a hard skull, but lacking limbs with digits. Fish can be grouped into the more basal jawless fish and the more common jawed fish, the latter including all living cartilaginous and bony fish, as well as the extinct placoderms and acanthodians.",
+      category: "FISH",
+      cover: "/p10.jpg",
+      date: "APRIL 10, 2024",
+    },
+    {
+      id: 8,
+      title: "One cat can just leads to another",
+      desc: "The cat, commonly referred to as the domestic cat or house cat, is the only domesticated species in the family Felidae. Recent advances in archaeology and genetics have shown that the domestication of the cat occurred in the Near East around 7500 BC",
+      category: "Cat",
+      cover: "/p15.jpg",
+      date:  "MARCH 28 2024",
+    },
+    {
+      id: 9,
+      title: "One cat can just leads to another",
+      desc: "The cat, commonly referred to as the domestic cat or house cat, is the only domesticated species in the family Felidae. Recent advances in archaeology and genetics have shown that the domestication of the cat occurred in the Near East around 7500 BC",
+      category: "Cat",
+      cover: "/p1.jpg",
+      date:  "MARCH 20 2024",
+    },
+    
+    
+  ]
+  
